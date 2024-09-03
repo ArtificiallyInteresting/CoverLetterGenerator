2ea810cee8a711aaf527058104fb52ad10099056
Henry Smith <artificiallyinteresting@gmail.com>
2024-09-03 11:32:07 -0700

    No more vite

--- client/src/Document.tsx
+++ client/src/Document.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import './Document.css'; 
 
-const API_URL = import.meta.env.VITE_API_URL;
+const API_URL = process.env.REACT_APP_API_URL;
 
 const Document: React.FC = () => {
   const [document, setDocument] = useState('');
